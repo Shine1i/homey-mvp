ce31eef6138f10e4af8d76199763a344145f5acc
wasim <wasimysdev@gmail.com>
2024-08-07 03:45:30 +0200

    project init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../app.pcss";
+</script>
+<main class="flex  flex-col overflow-hidden justify-between w-dvw min-h-dvh bg-black">
+  <slot />
+<!--  <nav class="fixed bottom-0 w-full h-24 bg-red-500 bg-nav-background shadow-lg">-->
+<!--   d-->
+<!--    -->
+<!--  </nav>-->
+
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="flex flex-col items-center justify-center relative min-h-screen">
+  <div class="relative flex-grow w-full">
+    <div class="absolute top-0 -left-4 w-72 h-72 bg-gray-500 rounded-full filter blur-xl opacity-70"></div>
+    <div class="absolute top-0 -right-4 w-72 h-72 bg-white rounded-full filter blur-xl opacity-70 animation-delay-2000"></div>
+    
+    <div class="flex flex-col items-center justify-center h-full">
+      <img src="/Isometric3D.png" class="z-10 min-w-[600px] relative" alt="">
+      <h1 class="text-white text-4xl">Homie</h1>
+    </div>
+  </div>
+  
+  <div class="text-white w-full text-center p-2 flex flex-col gap-2">
+    <button type="button" class="rounded-full w-full bg-yellow-200 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm focus:bg-yellow-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign up</button>
+    <button type="button" class="rounded-full w-full bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Log in</button>
+  
+  </div>
+</div>
