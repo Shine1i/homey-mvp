89adbc208a6b058c49dae8076de7230f08fd5f74
wasim <wasimysdev@gmail.com>
2024-08-07 06:23:48 +0200

    Refactor page layout with 3D canvas and animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,56 @@
-<div class="flex flex-col items-center justify-center relative min-h-screen">
-  <div class="relative flex-grow w-full">
-    <div class="absolute top-0 -left-4 w-72 h-72 bg-gray-500 rounded-full filter blur-xl opacity-70"></div>
-    <div class="absolute top-0 -right-4 w-72 h-72 bg-white rounded-full filter blur-xl opacity-70 animation-delay-2000"></div>
+<script lang="ts">
+    import { Application } from "@splinetool/runtime";
+    import { fly, fade } from 'svelte/transition';
     
-    <div class="flex flex-col items-center justify-center h-full">
-      <img src="/Isometric3D.png" class="z-10 min-w-[600px] relative" alt="">
-      <h1 class="text-white text-4xl">Homie</h1>
+    let showText = $state(false);
+    let showBlob = $state(false);
+    
+    $effect(() => {
+        const canvas = document.getElementById('canvas3d');
+        const app = new Application(canvas as HTMLCanvasElement);
+        
+        app
+          .load('https://prod.spline.design/DOrVpQUMc6dpaqD9/scene.splinecode')
+          .then(()=>{
+              app.setZoom(0.5);
+              showText = true;
+              showBlob = true;
+              
+          });
+    });
+</script>
+
+<div class="flex flex-col justify-between h-screen">
+  <div class="flex-grow flex flex-col items-center relative justify-center">
+    <div class="min-h-80 relative">
+      <canvas id="canvas3d" class="w-full pointer-events-none left-4 z-10 relative !max-h-80"></canvas>
+      {#if showBlob}
+        <div
+            in:fade={{ duration: 1000 }}
+            class="absolute -bottom-3 left-1/2 transform -translate-x-1/2 w-72 h-72 bg-purple-300  rounded-full filter blur-xl opacity-70"
+        ></div>
+      {/if}
     </div>
+      {#if showText}
+        <div
+            in:fly={{ x: -300, duration: 1000 }}
+            class="text-5xl font-bold tracking-tighter text-purple-200 mt-12 text-center"
+        >
+          h<span class="">o</span>m<span class="inline-block text-amber-400 animate-bounce">i</span>e
+        </div>
+      {/if}
   </div>
   
-  <div class="text-white w-full text-center p-2 flex flex-col gap-2">
-    <button type="button" class="rounded-full w-full bg-yellow-200 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm focus:bg-yellow-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign up</button>
-    <button type="button" class="rounded-full w-full bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Log in</button>
-  
+  <div class="w-full p-4 space-y-2">
+    <button type="button" class="w-full rounded-full bg-purple-200 px-3.5 py-3.5 text-sm font-semibold text-black shadow-sm hover:bg-yellow-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign up</button>
+    <button type="button" class="w-full rounded-full bg-amber-400 px-3.5 py-3.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Log in</button>
   </div>
 </div>
+
+<style>
+    #canvas3d {
+        max-width: 100dvw;
+        max-height: 70vh;
+        object-fit: contain;
+    }
+</style>
